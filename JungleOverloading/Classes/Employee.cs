cb43b29b3f581946b20c98833498095e1c192e22
Emily DeWitt <47046126+emilykdewitt@users.noreply.github.com>
2019-09-06 18:01:08 -0700

    Completed exercise

---
+++ JungleOverloading/Classes/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JungleOverloading.Classes
+{
+    class Employee
+    {
+        string[] restaurants = { "McDonald's", "Wendy's", "Taco Bell", "Chick-Fil-A", "Pizza Hut" };
+
+        string[] coworkers = { "Jim", "Pam", "Creed", "Michael", "Dwight" };
+
+        public string first { get; set; }
+
+        public string last { get; set; }
+
+        public Employee(string firstName, string lastName)
+        {
+            first = firstName;
+            last = lastName;
+        }
+
+        List<string> myRestaurants = new List<string>();
+
+        public void MakeRestaurantList(string restaurant)
+        {
+            myRestaurants.Add(restaurant);
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            Console.WriteLine("Time for a lunch break");
+            Eat(random.Next(min, max));
+            return random.Next(min, max);
+        }
+
+        public string Eat(int index)
+        {
+            Random rand = new Random();
+            int indexNum = rand.Next(restaurants.Length);
+            Console.WriteLine($"{first} {last} likes to go to {restaurants[indexNum]}");
+            return restaurants[index];
+        }
+
+        public string Eat(string meal)
+        {
+            Console.WriteLine($"{first} is eating {meal} at the office");
+            return meal;
+        }
+
+        public void Eat(List<Employee> companions, string meal)
+        {
+            Console.Write($"{first} is out to lunch with ");
+
+            foreach (var companion in companions)
+            {
+                string firstNameCompanion = companion.first;
+                Console.Write($"{companion.first}, and ");
+            }
+            Console.Write($" and eating {meal}");
+        }
+
+    }
+}
---
+++ JungleOverloading/Program.cs
@@ -0,0 +1,48 @@
+﻿using JungleOverloading.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace JungleOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first and last name.");
+            var getFullName = Console.ReadLine();
+
+            string[] splitFirstLastName = getFullName.Split(" ");
+
+            var employee = new Employee(splitFirstLastName[0], splitFirstLastName[1]);
+
+            Console.WriteLine("What's are some restaurants in your area?");
+            var aRestaurant = Console.ReadLine();
+            String[] allRestaurants = aRestaurant.Split(", ");
+            foreach (string restaurant in allRestaurants)
+            {
+                Console.WriteLine(restaurant);
+                employee.MakeRestaurantList(restaurant);
+            };
+
+            employee.RandomNumber(1, allRestaurants.Length);
+
+            Console.WriteLine("What do you want to eat at that restaurant?");
+            var whatEmployeeWillEat = Console.ReadLine();
+
+            employee.Eat(whatEmployeeWillEat);
+
+            List<Employee> companions = new List<Employee>();
+            Console.WriteLine("Type the first and last names of some people you work with.");
+            var myCoWorkers = Console.ReadLine();
+            string[] teammates = myCoWorkers.Split(", ");
+            foreach (var teammateName in teammates)
+            {
+                string[] firstLast = teammateName.Split(" ");
+                var newEmployee = new Employee(firstLast[0], firstLast[1]);
+                companions.Add(newEmployee);
+            }
+
+            employee.Eat(companions, whatEmployeeWillEat);
+        }
+    }
+}
